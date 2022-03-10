f568faea8b600e2326a2854604771d3219066da8
Johanny <johanny_andrade@yahoo.com.br>
2022-03-09 21:53:59 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,26 @@
+
+import{ReactComponent as GitIcon} from 'assets/img/github.svg';
+import './styles.css'
+
+function Navbar() {
+
+    return (
+
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="htpp://github.com/devsuperior">
+            <div className="dsmovie-contact-container">
+              <GitIcon />
+              <p className="dsmovie-contact-link">/devsuperior</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+
+    );
+}
+
+export default Navbar;
